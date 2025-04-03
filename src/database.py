eb0fd34f7205cf004c46bceab5186161e396964e
Jorg Janssen <jorg.janssen@han.nl>
2025-04-03 17:14:51 +0200

    Update database.py
    NOT NULL toegevoegd aan PK-kolommen vanwege Codd, E. F. (1990). Missing Information. The Relational Model for Database Management: Version, 2, 8-9.

--- src/database.py
+++ src/database.py
@@ -12,12 +12,12 @@ def haal_databaseverbinding_op():
 
     # voor het geval dit de eerste keer is, anders doen de statements niets
     db.execute('''CREATE TABLE IF NOT EXISTS persoon (
-                        naam VARCHAR(50) PRIMARY KEY, 
+                        naam VARCHAR(50) NOT NULL PRIMARY KEY, 
                         afstand NUMERIC(3,1)
                      )''')
     db.execute('''CREATE TABLE IF NOT EXISTS hobby (
-                        naam VARCHAR(50), 
-                        hobby VARCHAR(75), 
+                        naam VARCHAR(50) NOT NULL, 
+                        hobby VARCHAR(75) NOT NULL, 
                         PRIMARY KEY(naam, hobby), 
                         FOREIGN KEY(naam) REFERENCES persoon(naam)
                      )''')

65cac9f57d0d8bf692c7ec97404326cc43269118
Jorg Visch <1082961+jvisch@users.noreply.github.com>
2025-04-04 13:22:14 +0200

    autocommit

--- src/database.py
+++ src/database.py
@@ -1,12 +1,11 @@
 '''Database component voor Eindapplicatie voor P1'''
 import sqlite3
 
+
 def haal_databaseverbinding_op():
     '''Functie om verbinding te maken met database'''
+    connectie = sqlite3.connect('db/data.db', autocommit=True)
 
-    # isolation_level=None zorgt ervoor dat de database in autocommit mode is
-    connectie = sqlite3.connect('db/data.db', isolation_level=None)
-        
     # feitelijk gebruiken we een cursor, maar we gebruiken het als een databaseverbinding
     db = connectie.cursor()
 
@@ -23,9 +22,11 @@ def haal_databaseverbinding_op():
                      )''')
     return db
 
+
 def verbreek_verbinding_met_database(db):
     '''Functie om verbinding met database te verbreken'''
     db.close()
+
 
 def persoon_aanwezig_in_database(naam):
     '''Functie om  te controleren of persoon in de database bestaat'''
@@ -35,10 +36,12 @@ def persoon_aanwezig_in_database(naam):
     verbreek_verbinding_met_database(db)
     return row is not None
 
+
 def persoon_hobby_combinatie_aanwezig_in_database(naam, hobby):
     '''Functie om te controleren of persoon met hobby in de database bestaat'''
     db = haal_databaseverbinding_op()
-    db.execute('SELECT naam FROM hobby WHERE naam = ? and hobby = ?', (naam, hobby))
+    db.execute(
+        'SELECT naam FROM hobby WHERE naam = ? and hobby = ?', (naam, hobby))
     row = db.fetchone()
     verbreek_verbinding_met_database(db)
     return row is not None
